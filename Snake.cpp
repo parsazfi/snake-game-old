8b26904fd621b6235627a00f7bccb1a1cc6c0973
Parsa.zf <parsazf8@gmail.com>
2023-01-11 04:04:56 +0330

    Add Game Over and Pause Mechanism, Make the game responsive with the resolution of window, make the game more customizable with variable and fix some bugs

--- Snake.cpp
+++ Snake.cpp
@@ -1,3 +1,9 @@
+/*
+Name Snake Game 
+Author: Parsazfi
+Year: 2023
+*/
+
 #include <graphics.h>
 #include <conio.h>
 
@@ -6,52 +12,61 @@ int const PrizeColor = 10;
 int const SnakeColor = COLOR(255, 0, 0);
 int const TextStartColor = COLOR(255, 128, 128);
 int const TextStartUpColor = COLOR(255, 0, 0);
-int const BackgroundColor = COLOR(0, 0, 0);
+int const TextStyle1Color = COLOR(255, 0, 0);
+int const TextStyle2Color = COLOR(0, 0, 0);
+int const BackgroundColor = COLOR(35, 35, 35);
+int const PanelColor = COLOR(255, 128, 128);;
 
-int const speed = 220;
-int const maxSpeed = 100;
+int const firstSpeed = 160;
+int const maxSpeed = 40;
+int const speedRate = 5;
 int const MAX_SIZE = 1000;
 int const LEN = 5;
 int r = 5;
 int len;
 int dist = 2 * r;
-int sX = dist; //LeftRight
+int sX = dist; // Left and Right
 int prizeX = -1, prizeY = -1;
-int sY = 0; //UpDown
+int sY = 0; // Up and Down
+int _delay;
+bool isPlaying;
 
 void StartUp()
 {
-    setfillstyle(1, BackgroundColor);
+	setfillstyle(1, BackgroundColor);
+	setbkcolor(BackgroundColor);
+	bar(0, 0, WIDTH, HEIGHT);
 	settextjustify(1, 1);
 	settextstyle(2, 0, 4);
 	setcolor(2);
-	outtextxy(350, 315, (char*)"Created by Parsa.zf");
+	outtextxy(WIDTH - 50, HEIGHT - 15, (char*)"Created by Parsa.zf");
 	setcolor(TextStartUpColor);
 	settextstyle(5, 0, 1);
-	outtextxy(80, 160, (char*)"The Red");
+	outtextxy(80, HEIGHT / 2, (char*)"The Red");
 	delay(500);
 	settextstyle(4, 0, 5);
-	outtextxy(120, 140, (char*)"S");
+	outtextxy(WIDTH / 2 - 82, HEIGHT / 2 - 25, (char*)"S");
 	delay(350);
-	outtextxy(162, 140, (char*)"n");
+	outtextxy(WIDTH / 2 - 40, HEIGHT / 2 - 25, (char*)"n");
 	delay(350);
-	outtextxy(201, 140, (char*)"a");
+	outtextxy(WIDTH / 2 - 0,  HEIGHT / 2 - 25, (char*)"a");
 	delay(350);
-	outtextxy(240, 140, (char*)"k");
+	outtextxy(WIDTH / 2 + 40, HEIGHT / 2 - 25, (char*)"k");
 	delay(350);
-	outtextxy(279, 140, (char*)"e");
+	outtextxy(WIDTH / 2 + 80, HEIGHT / 2 - 25, (char*)"e");
 	delay(150);
 	setcolor(TextStartColor);
 
 	while (!kbhit())
 	{
+		//215
 		settextstyle(3, 0, 3);
-		outtextxy(200, 215, (char*)"..: Start :..");
+		outtextxy(WIDTH / 2, HEIGHT / 1.53, (char*)"..: Start :..");
 		settextstyle(8, 0, 1);
-		outtextxy(200, 235, (char*)"Press any key");
+		outtextxy(WIDTH / 2, HEIGHT / 1.53 + 20, (char*)"Press any key");
 		delay(400);
 
-		bar(50, 180, 280, 300);
+		bar(WIDTH / 2 - 100, HEIGHT / 1.53 - 20, WIDTH / 2 + 100, HEIGHT / 1.53 + 50);
 		
 		if (kbhit()) break; // for more speed
 		delay(450);
@@ -72,15 +87,65 @@ void CheckPrize(int x, int y)
 	if ((x == prizeX) && (y == prizeY))
 	{
 		len++;
+		if (_delay > maxSpeed) _delay -= speedRate;
 		setfillstyle(9, BackgroundColor);
 		fillellipse(prizeX, prizeY , r+1, r+1);
 		GeneratePrize();
 	}
 }
 
-void SnakeMoving (int x[], int y[], char state = '0') // r: right, l: left, u: up, d: down, 0: nothing
+void GameOver (int x[], int y[])
 {
-	if (len == -1)
+	bool flag = false;
+	for (int i = 0; i < 8; i++)
+	{
+		if (flag) setfillstyle(1, SnakeColor);
+		else setfillstyle(1, BackgroundColor);
+
+		for (int j = 0; j < len; j++)
+			fillellipse(x[j], y[j] ,r ,r);
+
+		flag = !flag;
+		delay (300);
+	}
+
+	delay (300);
+	setfillstyle(1, PanelColor);
+	bar (WIDTH / 2 - 135, HEIGHT / 2 - 75, WIDTH / 2 + 135, HEIGHT / 2 + 75);
+
+	setfillstyle(1, BackgroundColor);
+	rectangle (WIDTH / 2 - 130, HEIGHT / 2 - 70, WIDTH / 2 + 130, HEIGHT / 2 + 70);
+
+	delay (500);
+	setbkcolor(PanelColor);
+	setcolor(TextStyle2Color);
+	settextstyle(10, 0, 5);
+	outtextxy(WIDTH / 2, HEIGHT / 2, (char*)"Game Over");
+	settextstyle(8, 0, 1);
+	outtextxy(WIDTH / 2, HEIGHT / 2 + 30, (char*)"Press R to reset");
+}
+
+void CheckBite (int x[], int y[])
+{
+	for (int i = 1; i < len; i++)
+	{
+		if ((x[i] == x[0]) && (y[i] == y[0]))
+		{
+			isPlaying = false;
+			GameOver(x, y);
+			while (true)
+			{
+				if (kbhit() && getch() == 'r')
+					break;
+			}
+			break;
+		}
+	}
+}
+
+void SnakeMoving (int x[], int y[])
+{
+	if (len == -1) // Run in first time
 	{
 		len = LEN;
 
@@ -99,9 +164,9 @@ void SnakeMoving (int x[], int y[], char state = '0') // r: right, l: left, u: u
 
 		GeneratePrize();
 	}
-	else
+	else // run after first time
 	{
-		setfillstyle(1, 0);
+		setfillstyle(1, BackgroundColor);
 		fillellipse(x[len-1], y[len-1] ,r ,r);
 		for (int i = len - 1; i > 0; i--)
 		{		
@@ -121,6 +186,8 @@ void SnakeMoving (int x[], int y[], char state = '0') // r: right, l: left, u: u
 
 		setfillstyle(1, SnakeColor);
 		fillellipse(x[0], y[0] ,r ,r);
+
+		CheckBite(x, y);
 	}
 
 }
@@ -153,9 +220,38 @@ void ChangeDirection(int code)
 	}
 }
 
+void Pause(int x[], int y[])
+{
+	setfillstyle(1, PanelColor);
+	bar (WIDTH / 2 - 135, HEIGHT / 2 - 75, WIDTH / 2 + 135, HEIGHT / 2 + 75);
+	setfillstyle(1, BackgroundColor);
+	rectangle (WIDTH / 2 - 130, HEIGHT / 2 - 70, WIDTH / 2 + 130, HEIGHT / 2 + 70);
+	setbkcolor(PanelColor);
+	setcolor(TextStyle2Color);
+	settextstyle(10, 0, 5);
+	outtextxy(WIDTH / 2, HEIGHT / 2, (char*)"Pause");
+	settextstyle(3, 0, 1);
+	outtextxy(WIDTH / 2, HEIGHT / 2 + 30, (char*)"Press any key to resume");
+
+	getch();
+
+	setbkcolor(BackgroundColor);
+	setcolor (BackgroundColor);
+	setfillstyle(1, BackgroundColor);
+	bar(0, 0, WIDTH, HEIGHT);
+	
+	setfillstyle(1, SnakeColor);
+	for (int i = 0; i < len; i++)
+		fillellipse(x[i], y[i] ,r ,r);
+
+	setfillstyle(9, PrizeColor);
+	fillellipse(prizeX, prizeY , r+1, r+1);
+}
+
 void Game ()
 {
-	setcolor(BackgroundColor);
+	setbkcolor (BackgroundColor);
+	setcolor (BackgroundColor);
 	setfillstyle(1, BackgroundColor);
 	bar(0, 0, WIDTH, HEIGHT);
 	delay (1000);
@@ -165,14 +261,18 @@ void Game ()
 	int x [MAX_SIZE] = {MAX_SIZE};
 	int y [MAX_SIZE] = {MAX_SIZE};
 	
-	int  _delay = speed;
+	_delay = firstSpeed;
 	len = -1;
 
-	bool isPlaying = true;
+	isPlaying = true;
 	while (isPlaying)
 	{
 		if (kbhit())
-			ChangeDirection(key = getch());
+			if ((key = getch()) == 'p')
+				Pause(x, y);
+			else
+				ChangeDirection(key);
+
 		SnakeMoving(x, y);
 		delay (_delay); 
 	}
@@ -187,8 +287,8 @@ int main(int argc, char const *argv[])
     // Start up window
     StartUp();
 
-	//Game
-	Game();
+	while (true)
+		Game();
 
     return 0;
 }
